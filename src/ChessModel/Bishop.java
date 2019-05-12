b44fa8098c4df8cf6ed1f16d4d4252819f3d9e51
ionutGMcatelina <49817734+ionutGMcatelina@users.noreply.github.com>
2019-05-12 11:07:06 +0300

    Add files via upload

--- src/ChessModel/Bishop.java
+++ src/ChessModel/Bishop.java
@@ -6,6 +6,13 @@ public class Bishop extends Chessman {
         setNume( "\u265D");
     }
 
+    /**
+     * Verifica daca pozitia pe care urmeaza sa fie mutata piesa se afla pe diagonala principala
+     * din care face parte pozitia curenta.
+     * @param x Linia pozitiei de verificat.
+     * @param y Coloana pozitiei de verificat.
+     * @return True daca conditia este inceplinita, false in caz contrar.
+     */
     private boolean freePd(int x, int y){
         int thisX = getX();
         if (x < thisX){
@@ -25,6 +32,13 @@ public class Bishop extends Chessman {
         return true;
     }
 
+    /**
+     * Verifica daca pozitia pe care urmeaza sa fie mutata piesa se afla pe diagonala secundara
+     * din care face parte pozitia curenta.
+     * @param x Linia pozitiei de verificat.
+     * @param y Coloana pozitiei de verificat.
+     * @return True daca conditia este inceplinita, false in caz contrar.
+     */
     private boolean freeSd(int x, int y){
         int thisX = getX();
         if (x < thisX){
@@ -44,14 +58,18 @@ public class Bishop extends Chessman {
         return true;
     }
 
+    /**
+     * Muta piesa pe o alta pozitie.
+     * @param x Linia pe care va fi mutat.
+     * @param y Coloana pe care va fi mutat.
+     * @return True daca s-a putut face mutarea, false in caz contrar.
+     */
     @Override
     public boolean move(int x, int y) {
         if (Math.abs(x - getX()) == Math.abs(y - getY())){
             if (x < getX() && y < getY() || x > getX() && y > getY()){
                 if (freePd(x, y)){
-                    if (swap(x, y))
-                        return true;
-                    return false;
+                    return swap(x, y);
                 }
                 else{
                     System.out.println("Mutare invalida!");
@@ -60,9 +78,7 @@ public class Bishop extends Chessman {
             }
             else{
                 if (freeSd(x, y)){
-                    if (swap(x, y))
-                        return true;
-                    return false;
+                    return swap(x, y);
                 }
                 else{
                     System.out.println("Mutare invalida!");
