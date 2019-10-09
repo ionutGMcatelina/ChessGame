f9be3d2a0b9cfa406c08657548f37add7345277f
ionutGMcatelina <49817734+ionutGMcatelina@users.noreply.github.com>
2019-10-09 17:40:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         SahModel model = new SahModel();
         SettingsView settingsView = new SettingsView();
         SahView view = new SahView(settingsView);
-        SahController controller = new SahController(model, view, settingsView);
+        new SahController(model, view, settingsView);
 
         view.setSize(1400, 800);
         view.setVisible(true);
