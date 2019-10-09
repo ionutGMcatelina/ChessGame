05de7d7009409b560058a47f0f7f5f65612e5ef5
ionutGMcatelina <49817734+ionutGMcatelina@users.noreply.github.com>
2019-10-09 17:52:33 +0300

    Update Knight.java

--- src/ChessModel/Knight.java
+++ src/ChessModel/Knight.java
@@ -6,12 +6,6 @@ public class Knight extends Chessman{
         setNume( "\u265E");
     }
 
-    /**
-     * Muta piesa pe o alta pozitie.
-     * @param x Linia pe care va fi mutat.
-     * @param y Coloana pe care va fi mutat.
-     * @return True daca s-a putut face mutarea, false in caz contrar.
-     */
     public boolean move(int x, int y){
         if ((tabla[x][y] instanceof Empty || !tabla[x][y].getColor().equals(getColor()))) {
             if (x == getX() - 2 || x == getX() + 2) {
@@ -28,10 +22,10 @@ public class Knight extends Chessman{
                     return false;
                 }
             }
-            System.out.println("Mutare invalida!");
+            System.out.println("Wrong move!");
             return false;
         }
-        System.out.println("Mutare invalida!");
+        System.out.println("Wrong move!");
         return false;
     }
 }
